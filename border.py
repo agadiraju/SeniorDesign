57eb180d80668548c8a66f7afc4b711172fab433
Sneha Keshwani <snehak@wharton.upenn.edu>
2015-04-21 22:49:14 -0400

    fixed merge

--- border.py
+++ border.py
@@ -2,7 +2,7 @@ import sys
 import math
 import numpy as np
 from PIL import Image, ImageOps, ImageMath
-<<<<<<< HEAD
+
 import imageSegmentation as IS
 import cv2
 
@@ -59,18 +59,4 @@ if __name__ == '__main__':
 	print gof
 	cv2.imshow("window title", img)
 	cv2.waitKey()
-=======
-import imageSegmentation
-import cv2
 
-if __name__ == '__main__':
-	input_filename = sys.argv[1]
-	im = Image.open(input_filename)
-	[x, y] = im.size
-	img = cv2.imread(input_filename)
-	gray = cv2.imread(input_filename,0)
-	ret,thresh = cv2.threshold(gray,127,255,1)
-	contours,h = cv2.findContours(thresh,1,2)
-# 		cv2.drawContours(img, contours, -1, (0,255,0), 3)
-# 		cv2.imshow("window title", img)
->>>>>>> 020a26acaa81dd1d7ffb4657cdb0edec47c560bd
