e6b2fe3af30a2bd8f7b53cebf296effbb57266be
Abhishek Gadiraju <abhishek.gadiraju@gmail.com>
2015-04-22 01:47:12 -0400

    rfc done

--- build_model.py
+++ build_model.py
@@ -6,9 +6,24 @@ import os
 import symmetry
 import sys
 
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+
+
+def build_random_forest_classfier():
+  X, y = build_feature_vector()
+  X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2,
+                                     random_state = 0)
+
+  print len(X_test)
+  clf = RandomForestClassifier(n_estimators = 5)
+  clf = clf.fit(X_train, y_train)
+  print clf.score(X_test, y_test)
+
 def build_feature_vector():
   img_list = import_image_paths()
   feature_vector = []
+  target_vector = []
 
   for img_filename in img_list:
     img = cv2.imread(img_filename)
@@ -31,20 +46,32 @@ def build_feature_vector():
     current_vec = [symmetry_ssim, symmetry_mse, border_gof, color_contrast]
     feature_vector.append(current_vec)
 
-  return np.array(feature_vector).shape
+    if 'benign' in img_filename:
+      target = 0
+    if 'malignant' in img_filename:
+      target = 1
+    target_vector.append(target)
+
+  #print target_vector
+  return (np.array(feature_vector), np.array(target_vector))
 
 def import_image_paths():
   path_list = ["img/benign", "img/malignant"]
   img_path_list = []
 
   for p in path_list:
+    counter = 0
     listing = os.listdir(p)
     for img in listing:
       img_path_list.append(p + '/' + img)
+      counter += 1
+
+      if counter == 25:
+        break
 
   return img_path_list
 
 
 if __name__ == '__main__':
-  print build_feature_vector()
+  build_random_forest_classfier()
 
