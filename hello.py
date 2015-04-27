6d44a8bbdb28f967f078c852dc746b994d45bee1
Abhishek Gadiraju <abhishek.gadiraju@gmail.com>
2015-04-27 00:42:07 -0400

    flask logging errors to stdout

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
+import logging
 import os
 
 from flask import Flask, request
 from analyze_image import classify_image_url
 
 app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 # heee
 @app.route('/')
